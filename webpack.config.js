11f8666af4779d0727eb11c339de6bc8acadb851
Cedric AUDRIT <cedric@audrit.be>
2021-12-20 19:55:40 +0100

    fix dep broken css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const Encore = require('@symfony/webpack-encore');
-
+// const path = require('path');
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
@@ -56,8 +56,14 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
-
+    .enableSassLoader()
+    .enablePostCssLoader()
+    // .enablePostCssLoader((options) => {
+    //     options.postcssOptions = {
+    //          // the directory where the postcss.config.js file is stored
+    //          config: path.resolve(__dirname, 'sub-dir', 'custom.config.js'),
+    //      };
+    //      })
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
